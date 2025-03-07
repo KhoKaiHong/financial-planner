f920ffce26c2be74c073b44ec3d83ccf990e08db
Kho Kai Hong <khokaihong@gmail.com>
2025-03-07 14:29:43 +0800

    added theming

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,32 @@
 import { Button } from "~/components/ui/button";
-import { Alert, Text, View } from "react-native";
+import { Text } from "~/components/ui/text";
+import { View } from "react-native";
+import { Sun } from "~/lib/icons/Sun";
+import { MoonStar } from "~/lib/icons/MoonStar";
+import { useColorScheme } from "~/lib/useColorScheme";
 
 export default function Index() {
+  const { toggleColorScheme, colorScheme } = useColorScheme();
+
   return (
     <View className="flex-1 items-center justify-center gap-y-2">
       <View className="items-center">
         <Text className="text-4xl">Welcome to NativeWind!</Text>
         <Text className="text-xl">Style your app with</Text>
-        <Text className="text-3xl bg-yellow-100 font-bold underline">
-          Tailwind CSS!
-        </Text>
+        <Text className="text-3xl font-bold underline">Tailwind CSS!</Text>
       </View>
       <Button
+        size={"icon"}
+        variant={"outline"}
         onPress={() => {
-          Alert.alert("NativeWind", "You're all set up!");
+          toggleColorScheme();
         }}
       >
-        <Text>Default</Text>
+        {colorScheme === "dark" ? (
+          <MoonStar className="text-foreground" />
+        ) : (
+          <Sun className="text-foreground" />
+        )}
       </Button>
     </View>
   );
