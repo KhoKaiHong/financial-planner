9249864da4b30d77822417e2470398419fc36f9a
Kho Kai Hong <khokaihong@gmail.com>
2025-03-30 23:53:57 +0800

    maps autocomplete done

--- app/(tabs)/maps.tsx
+++ app/(tabs)/maps.tsx
@@ -1,4 +1,4 @@
-import { Platform, View } from "react-native";
+import { ScrollView, View } from "react-native";
 import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
 import { Text } from "~/components/ui/text";
 import { useLocation } from "~/hooks/useLocation";
@@ -15,9 +15,7 @@ import { Button } from "~/components/ui/button";
 import { Separator } from "~/components/ui/separator";
 import { TriggerRef } from "@rn-primitives/popover";
 import { Input } from "~/components/ui/input";
-import { useDebounceCallback } from "usehooks-ts";
-import { Switch } from "~/components/ui/switch";
-import { Label } from "~/components/ui/label";
+import { useDebounce } from "@uidotdev/usehooks";
 
 type Coordinates = {
   latitude: number;
@@ -38,10 +36,6 @@ type autocompleteCurrentLocationbody = {
       radius: 5000.0;
     };
   };
-  origin?: {
-    latitude: number;
-    longitude: number;
-  };
 };
 
 type autocompleteResult = {
@@ -57,6 +51,7 @@ type autocompleteResult = {
   }[];
 };
 
+// async function that fetches the autocomplete location data from the Google Maps API
 async function autocompleteLocation(body: autocompleteCurrentLocationbody) {
   const resp = await fetch(
     "https://places.googleapis.com/v1/places:autocomplete",
@@ -81,37 +76,86 @@ async function autocompleteLocation(body: autocompleteCurrentLocationbody) {
   }
 }
 
+async function getCoordsByPlaceId(placeId: string) {
+  const resp = await fetch(
+    `https://places.googleapis.com/v1/places/${placeId}`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+        "X-Goog-Api-Key": process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY ?? "",
+        "X-Goog-FieldMask": "location",
+      },
+      method: "GET",
+    }
+  );
+
+  const result = await resp.json();
+
+  return result.location as Coordinates;
+}
+
 export default function Maps() {
-  const { data, isError, error, isSuccess } = useLocation();
+  const queryClient = useQueryClient();
+
+  const useLocationQuery = useLocation();
+
   const requestLocationMutation = requestLocation();
 
-  const queryClient = useQueryClient();
+  const originLocationMutation = useMutation({
+    mutationFn: (body: autocompleteCurrentLocationbody) => {
+      return autocompleteLocation(body);
+    },
+    mutationKey: ["originLocation"],
+  });
+
+  const getCoordsByPlaceIdMutation = useMutation({
+    mutationFn: getCoordsByPlaceId,
+    mutationKey: ["getCoordsByPlaceId"],
+  });
 
   const [coords, setCoords] = useState<Coordinates>({
     latitude: 3.1319,
     longitude: 101.6841,
   });
-  const [originLocation, setOriginLocation] = useState<Coordinates>(coords);
+  const [originLocation, setOriginLocation] = useState<Coordinates | null>(
+    null
+  );
 
-  const [originLocationSearch, setOriginLocationSearch] = useState("");
+  const [autoCompleteSuggestions, setAutoCompleteSuggestions] = useState<
+    { placeId: string; text: string }[]
+  >([]);
 
-  const originLocationbody: autocompleteCurrentLocationbody = {
-    input: "",
-    includeQueryPredictions: false,
-    includedRegionCodes: ["my"],
-    languageCode: "en",
-    ...(isSuccess && {
+  const [originLocationBody, setOriginLocationBody] =
+    useState<autocompleteCurrentLocationbody>({
+      input: "",
+      includeQueryPredictions: false,
+      includedRegionCodes: ["my"],
+      languageCode: "en",
+    });
+
+  const debouncedOriginLocationBody = useDebounce(originLocationBody, 500);
+
+  const addLocationBias = useCallback((latitude: number, longitude: number) => {
+    setOriginLocationBody((prevBody) => ({
+      ...prevBody,
       locationBias: {
         circle: {
           center: {
-            latitude: originLocation.latitude,
-            longitude: originLocation.longitude,
+            latitude: latitude,
+            longitude: longitude,
           },
           radius: 5000.0,
         },
       },
-    }),
-  };
+    }));
+  }, []);
+
+  const setOriginLocationBodyInput = useCallback((input: string) => {
+    setOriginLocationBody((prevBody) => ({
+      ...prevBody,
+      input: input,
+    }));
+  }, []);
 
   const triggerRef = useRef<TriggerRef>(null);
 
@@ -127,48 +171,54 @@ export default function Maps() {
     }
   }, [triggerRef]);
 
-  const [text, setText] = useState("Current Location");
+  const [originLocationInputText, setOriginLocationInputText] = useState("");
 
-  const debouncedMutate = useCallback(
-    useDebounceCallback((text: string) => {
-      if (text.length !== 0) {
-        originLocationMutation.mutate({
-          ...originLocationbody,
-          input: text,
-        });
-      }
-    }, 500),
-    []
-  );
+  useEffect(() => {
+    if (debouncedOriginLocationBody.input.length !== 0) {
+      originLocationMutation.mutate(debouncedOriginLocationBody, {
+        onSuccess: (data) => {
+          if (data === null) {
+            setAutoCompleteSuggestions([]);
+          } else {
+            setAutoCompleteSuggestions(
+              data.suggestions.map((suggestion) => {
+                return {
+                  placeId: suggestion.placePrediction.placeId,
+                  text: suggestion.placePrediction.structuredFormat.mainText
+                    .text,
+                };
+              })
+            );
+          }
+        },
+      });
+    }
+  }, [debouncedOriginLocationBody]);
 
   const handleTextChange = useCallback(
     (text: string) => {
-      const trimmed = text.trim();
-      setText(trimmed);
-      debouncedMutate(trimmed);
+      const trimmed = text.trimStart();
+      setOriginLocationInputText(trimmed);
+      setOriginLocationBodyInput(trimmed);
     },
-    [setText, debouncedMutate]
+    [setOriginLocationInputText]
   );
 
   const onPressText = () => {
-    if (text === "Current Location") {
-      setText("");
+    if (originLocationInputText === "Current Location") {
+      setOriginLocationInputText("");
     }
     openPopOver();
   };
 
-  const originLocationMutation = useMutation({
-    mutationFn: (body: autocompleteCurrentLocationbody) => {
-      return autocompleteLocation(body);
-    },
-    mutationKey: ["originLocation"],
-  });
-
   const region = { ...coords, latitudeDelta: 0.0922, longitudeDelta: 0.0421 };
 
   useEffect(() => {
-    if (isError && requestLocationMutation.isIdle) {
-      if ("code" in error && error.code === "ERR_LOCATION_UNAUTHORIZED") {
+    if (useLocationQuery.isError && requestLocationMutation.isIdle) {
+      if (
+        "code" in useLocationQuery.error &&
+        useLocationQuery.error.code === "ERR_LOCATION_UNAUTHORIZED"
+      ) {
         requestLocationMutation.mutate(undefined, {
           onSuccess: (data) => {
             if (data.granted) {
@@ -178,89 +228,93 @@ export default function Maps() {
         });
       }
     }
-  }, [isError]);
+  }, [useLocationQuery.isError]);
 
   useEffect(() => {
-    if (isSuccess) {
+    if (useLocationQuery.isSuccess) {
       setCoords({
-        latitude: data.coords.latitude,
-        longitude: data.coords.longitude,
+        latitude: useLocationQuery.data.coords.latitude,
+        longitude: useLocationQuery.data.coords.longitude,
       });
       setOriginLocation({
-        latitude: data.coords.latitude,
-        longitude: data.coords.longitude,
+        latitude: useLocationQuery.data.coords.latitude,
+        longitude: useLocationQuery.data.coords.longitude,
       });
-      setChecked(true);
+      setOriginLocationInputText("Current Location");
+      addLocationBias(
+        useLocationQuery.data.coords.latitude,
+        useLocationQuery.data.coords.longitude
+      );
     }
-  }, [isSuccess]);
-
-  const [checked, setChecked] = useState<boolean | null>(null);
+  }, [useLocationQuery.isSuccess]);
 
   return (
     <View className="flex-1">
-      <View className="flex-row justify-center py-2">
-        <Popover>
-          <PopoverTrigger ref={triggerRef} asChild>
-            <Input
-              value={text}
-              onChangeText={handleTextChange}
-              onFocus={onPressText}
-              onBlur={closePopOver}
-              className="w-80"
-              placeholder="Enter location"
-            ></Input>
-          </PopoverTrigger>
-          <PopoverContent side={"bottom"} className="w-80 flex px-0 py-0">
-            {originLocationMutation.data ? (
-              originLocationMutation.data.suggestions.map(
-                (suggestion, index, arr) => (
+      <ScrollView className="flex-1">
+        <View className="flex-row justify-center py-2">
+          <Popover>
+            <PopoverTrigger ref={triggerRef}>
+              <Input
+                value={originLocationInputText}
+                onChangeText={handleTextChange}
+                onFocus={onPressText}
+                onBlur={closePopOver}
+                className="w-80"
+                placeholder="Enter location"
+                autoCapitalize="none"
+                autoCorrect={false}
+                autoComplete="off"
+              ></Input>
+            </PopoverTrigger>
+            <PopoverContent side={"bottom"} className="w-80 flex px-0 py-0">
+              {autoCompleteSuggestions.length > 0 ? (
+                autoCompleteSuggestions.map((suggestion, index, arr) => (
                   <View>
                     <Button
                       key={"button" + index}
                       variant="ghost"
                       className="items-start h-auto py-3"
                       size="sm"
+                      onPress={() => {
+                        triggerRef.current?.close();
+                        setOriginLocationInputText(suggestion.text);
+                        getCoordsByPlaceIdMutation.mutate(suggestion.placeId, {
+                          onSuccess: (data) => {
+                            setOriginLocation(data);
+                          },
+                        });
+                      }}
                     >
-                      <Text key={"text" + index}>
-                        {
-                          suggestion.placePrediction.structuredFormat.mainText
-                            .text
-                        }
-                      </Text>
+                      <Text key={"text" + index}>{suggestion.text}</Text>
                     </Button>
                     {index != arr.length - 1 && (
                       <Separator className="bg-accent" />
                     )}
                   </View>
-                )
-              )
-            ) : (
-              <View className="py-3">
-                <Text className="text-muted-foreground self-center">
-                  No suggestions
-                </Text>
-              </View>
+                ))
+              ) : (
+                <View className="py-3">
+                  <Text className="text-muted-foreground self-center">
+                    No suggestions
+                  </Text>
+                </View>
+              )}
+            </PopoverContent>
+          </Popover>
+        </View>
+        <View className="h-72 flex rounded-3xl">
+          <MapView
+            style={{ flex: 1, width: "100%", height: "100%" }}
+            provider={PROVIDER_GOOGLE}
+            initialRegion={region}
+            region={region}
+          >
+            {originLocation !== null && (
+              <Marker coordinate={originLocation} pinColor="aqua" />
             )}
-          </PopoverContent>
-        </Popover>
-        <Switch
-          disabled={checked === null}
-          checked={checked ?? false}
-          onCheckedChange={setChecked}
-          nativeID="use-current-location"
-        />
-        <Label nativeID="use-current-location">Use Current Location</Label>
-      </View>
-      <View className="h-72 flex rounded-3xl">
-        <MapView
-          style={{ flex: 1, width: "100%", height: "100%" }}
-          provider={PROVIDER_GOOGLE}
-          initialRegion={region}
-          region={region}
-        >
-          <Marker coordinate={coords} pinColor="aqua" />
-        </MapView>
-      </View>
+          </MapView>
+        </View>
+      </ScrollView>
     </View>
   );
 }
